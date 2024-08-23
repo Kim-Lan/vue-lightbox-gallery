36ec14ee3ea6327e1e3cd68d094de9cbf07cc921
Kim-Lan <me@kim-lan.com>
2024-08-23 00:35:36 -0700

    Add more placeholder images

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,14 @@
 import LightboxGallery from './components/LightboxGallery.vue'
 
 const images = [
-  'https://picsum.photos/200/300',
-  'https://picsum.photos/200',
+  'https://picsum.photos/600/800',
+  'https://picsum.photos/600',
   'https://picsum.photos/2000/1500',
   'https://picsum.photos/2000/1000',
-  'https://picsum.photos/1000'
+  'https://picsum.photos/1000',
+  'https://picsum.photos/1920/1080',
+  'https://picsum.photos/1280/720',
+  'https://picsum.photos/2000/1200'
 ];
 </script>
 
