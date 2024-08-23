ab3ac878728a717740775c96e5d85a3e10092be3
Kim-Lan <me@kim-lan.com>
2024-08-22 20:31:32 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import LightBox from './components/LightBox.vue'
+</script>
+
+<template>
+  <header>
+
+  </header>
+
+  <main>
+    <LightBox />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/LightBox.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+  
+}>();
+</script>
+
+<template>
+  <div>
+    <h1></h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
