0a972734e4305f028a4844232839b39ec257e66c
Kim-Lan <me@kim-lan.com>
2024-08-22 21:24:06 -0700

    Add light box

--- src/components/LightBoxGallery.vue
+++ src/components/LightBoxGallery.vue
@@ -12,15 +12,37 @@ const props = withDefaults(defineProps<Props>(), {
 
 const maxSizeString = computed(() => props.maxSize + 'px');
 
-const visible = ref(false);
-const index = ref(0);
+const isLightboxVisible = ref(false);
+const currentImage = ref('');
+
+function showLightbox(index: number) {
+  isLightboxVisible.value = true;
+  currentImage.value = props.images[index];
+}
+
+function closeLightbox() {
+  isLightboxVisible.value = false;
+  currentImage.value = '';
+}
 </script>
 
 <template>
   <div class="gallery">
-    <div v-for="(image, index) in props.images" class="gallery-image">
+    <div
+      v-for="(image, index) in props.images"
+      class="gallery-image"
+      @click="showLightbox(index)"
+    >
       <img :src="image" />
     </div>
+  </div>
+
+  <div
+    v-if="isLightboxVisible"
+    class="lightbox"
+    @click.self="closeLightbox"
+  >
+    <img :src="currentImage" />
   </div>
 </template>
 
@@ -49,5 +71,15 @@ const index = ref(0);
   flex-shrink: 1;
   max-height: v-bind(maxSizeString);
   border-radius: 5px;
+}
+
+.lightbox {
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(10, 10, 10, 0.85);
+  z-index: 1000;
 }
 </style>
