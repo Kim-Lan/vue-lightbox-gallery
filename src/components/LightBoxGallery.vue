c8c705111ec52c11fb5347efff5337eafc60f97a
Kim-Lan <me@kim-lan.com>
2024-08-22 21:09:53 -0700

    Style gallery

--- src/components/LightBoxGallery.vue
+++ src/components/LightBoxGallery.vue
@@ -1,23 +1,53 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
-const props = defineProps<{
+export interface Props {
   images: string[]
-}>();
+  maxSize?: number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  maxSize: 300
+});
+
+const maxSizeString = computed(() => props.maxSize + 'px');
 
 const visible = ref(false);
 const index = ref(0);
 </script>
 
 <template>
-  <div>
-    <img
-      v-for="(image, index) in props.images"
-      :src="image"
-    />
+  <div class="gallery">
+    <div v-for="(image, index) in props.images" class="gallery-image">
+      <img :src="image" />
+    </div>
   </div>
 </template>
 
 <style scoped>
+.gallery {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 5px;
+  margin: 5px;
+}
 
+.gallery-image {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  flex-shrink: 1;
+  max-width: v-bind(maxSizeString);
+  cursor: pointer;
+}
+
+.gallery-image img {
+  display: block;
+  object-fit: cover;
+  flex-grow: 1;
+  flex-shrink: 1;
+  max-height: v-bind(maxSizeString);
+  border-radius: 5px;
+}
 </style>
